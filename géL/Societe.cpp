cf7c7e9ecae0d8e2db59ad2ee4f3845a49e8674a
Buresi Thomas <thomasburesi@me.com>
2020-06-02 18:39:08 +0200

    Ajout détails fonction Analyse

--- géL/Societe.cpp
+++ géL/Societe.cpp
@@ -1,5 +1,5 @@
 #include "societe.h"
-
+#include "Purificateur.h"
 Societe::Societe(long id)
 {
 	this->id = id;
@@ -7,5 +7,34 @@ Societe::Societe(long id)
 
 Societe::~Societe()
 {
+}
+
+
+int Societe::rechercher(int option, float r, float lon, float lat)
+{
+    switch(option)
+    {
+
+        case 1 :
+
+            return rechercherQualiteTerritoire(float r, float lon, float lat);
+
+        case 2 :
+            
+            return rechercherQualiteMoyenne(float r = NULL, float lon = NULL, float lat = NULL);
+            
+        default :
+            return 0;
+
+
+
+    }
+}
+
+int Societe::analyserImpactPurificateur(float r, long id, time_t dateDesiree)
+{
+    
+    
+    
 }
 
--- géL/Societe.h
+++ géL/Societe.h
@@ -2,17 +2,21 @@
 #define SOCIETE
 #include "Purificateur.h"
 #include <string>
+#include <ctime>
+
 using namespace std;
 class Societe {
 public:
 	Societe(long id);
 	~Societe();
 	string getId();
-	int analyserImpactPurificateur();
-	Purificateur rechercher();
+	int analyserImpactPurificateur(time_t dateDesiree);
+	int rechercher(int recherche, float r, float lon, float lat);
+
 
 private:
 	long id;
 
 };
 #endif SOCIETE
+
